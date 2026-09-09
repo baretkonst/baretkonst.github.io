1848e4b7b126f18e15df3ba0544eab6586958d15
bjorn <baretkonst@gmail.com>
2026-09-09 19:30:59 +0200

    Updated text

--- js/gallery_en.js
+++ js/gallery_en.js
@@ -132,7 +132,7 @@ function openLightboxGroup(groupNumber, activeArt) {
           : '<span ' + labelStyle + '><strong>Creation:</strong></span>Fixed structures<br>';
 
         var colorscaleHTML = formattedScale
-          ? '<span ' + labelStyle + '><strong>Färgskala:</strong></span>' + formattedScale + '<br>'
+          ? '<span ' + labelStyle + '><strong>Colorscale:</strong></span>' + formattedScale + '<br>'
           : '';
 
         printContainer.innerHTML =
